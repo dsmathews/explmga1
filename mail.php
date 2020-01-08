4e833e399b057cfcb1f7820ce416b608737aaf69
Darrell Mathews <smathewsbd@gmail.com>
2020-01-08 16:11:40 -0500

    added confirmation page

--- mail.php
+++ mail.php
@@ -1,14 +1,4 @@
 <?php
-   // Takes raw data from the request
-   $json = file_get_contents('php://input');
-
-   // Converts it into a PHP object
-   $data = json_decode($json);
-
-   echo $data;
-
-   // data sent in header are in JSON format
-   header('Content-Type: application/json');
    // takes the value from variables and Post the data
    $fname = $_POST["firstName"];
    $lname = $_POST['lastName'];
@@ -19,48 +9,48 @@
    $degree = $_POST['degreeConcentration'];
    $jobTitle = $_POST['jobTitle'];
    $employer = $_POST['employer'];
-   $photo = $_POST['photo'];
+   // $photo = $_POST['photo'];
    $jobDescriptiton = $_POST['jobDescription'];
    $receivedFromMGA = $_POST['receivedFromMGA'];
    $biography = $_POST['biography'];
    $agreement = $_POST['agreement']; 
-   $to = "darrell.mathews@mga.edu";
+   $to = "dsmathews@icloud.com";
    $subject = "Successful Alumni Form Data";
 
    
    // Email Template
-   $message = "<b>Name : </b>".$fname.$lname.$suff."<br>\n";
-   $message .= "<b>Phone Number : </b>".$phone."<br>\n";
-   $message .= "<b>Email Address : </b>".$email."<br>\n";
-   $message .= "<b>Graduation Year : </b>".$gradYear."<br>\n";
-   $message .= "<b>Degree : </b>".$degree."<br>\n";
-   $message .= "<b>Job Title : </b>".$jobTitle."<br>\n";
-   $message .= "<b>Employer : </b>".$employer."<br>\n";
-   $message .= "<b>Photo : </b><img src=".$photo."><br>\n";
-   $message .= "<b>Job Description : </b>".$jobDescriptiton."<br>\n";
-   $message .= "<b>How did MGA help me be to be successful? : </b>".$receivedFromMGA."<br>\n";
-   $message .= "<b>Biography : </b>".$biography."<br>\n";
-   $message .= "<b>Consent to use of information : </b>".$agreement."<br>\n";
+   $message = "<b>Name: </b>".$fname .$lname .$suff."\r\n";
+   $message .= "<b>Phone Number: </b>".$phone."\r\n";
+   $message .= "<b>Email Address : </b>".$email."\r\n";
+   $message .= "<b>Graduation Year : </b>".$gradYear."\r\n";
+   $message .= "<b>Degree : </b>".$degree."\r\n";
+   $message .= "<b>Job Title : </b>".$jobTitle."\r\n";
+   $message .= "<b>Employer : </b>".$employer."\r\n";
+   // $message .= "<b>Photo : </b><img src=".$photo."><br>\n";
+   $message .= "<b>Job Description : </b>".$jobDescriptiton."\r\n";
+   $message .= "<b>How did MGA help me be to be successful? : </b>".$receivedFromMGA."\r\n";
+   $message .= "<b>Biography : </b>".$biography."\r\n";
+   $message .= "<b>Consent to use of information : </b>".$agreement."\r\n";
    
-   $message = wordwrap($message, 70);
+   $messages = wordwrap($message, 70);
    
-   $header = "From:".$email."\r\n";
-   $header .= "MIME-Version: 1.0\r\n";
-   $header .= "Content-type: text/html\r\n";
-   $retval = mail($to,$subject,$message,$header);
-   // message Notification
-   if( $retval == true ) {
+   $headers = "From: $fname. $lname\r\n";
+   $headers .="Reply-to: $email. \r\n";
+   // $headers .= "MIME-Version: 1.0\r\n";
+   $headers .= "Content-type: text/html\r\n";
+   $val = mail($to,$subject,$messages,$headers);
+   if( $val == true ) {
+      header('Location: thank-you.html');
       echo json_encode(array(
          'success'=> true,
-         'message' => $to,$subject,$message,$header
+         'message' => 'Message sent successfully'
       ));
    }else {
-      echo "<pre>";
-      print_r($_POST);
-      echo "</pre>";
-      // echo json_encode(array(
-      //    'error'=> true,
-      //    'message' => $to,$subject,$message,$header
+      echo json_encode(array(
+         'error'=> true,
+         'message' => $to,$subject,$messages,$headers
 
-      // ));
-   };
+      ));
+   }
+   
+   // message Notification
